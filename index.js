d62a3cd76558690905ff9192d237b4340c30da20
maher <gomemahero.com>
2022-10-27 00:24:57 +0300

    redep

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 require('dotenv').config()
 const { Sequelize, Model, DataTypes } = require('sequelize')
 const express = require('express')
+const cors = require('cors')
+
 const app = express()
+app.use(cors());
+app.use(express.json());
+app.use(express.json({ extended: true }));
 
 // const sequelize = new Sequelize('mysql-93288-0.cloudclusters.net', {
 //     dialectOptions: {
